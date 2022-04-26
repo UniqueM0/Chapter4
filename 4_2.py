43ffa0821faad2dee1788405139711a6e0688956
Unique Orduna <uniqueorduna@swuser-129-8-204-222.swlan.csufresno.edu>
2022-04-26 15:17:02 -0700

    finish 4_2

---
+++ 4_2.py
@@ -0,0 +1,60 @@
+
+# def main():
+#     first()
+#     print(str(4) + ": from function main")
+
+# def first():
+#     print(str(1) + ": from function first")
+#     second()
+#     print(str(3) + ": from functin fisrt")
+
+# def second():
+#     print(str(2) + ": from function second")
+
+# main()
+
+
+
+# list1 = ["a", "e", "i", "o", "u"]    
+# # list2 = []
+# def f(y):
+#     return y.upper()
+
+# #dont need 
+# # for x in list1:
+# #     print("x: ", x)
+# #     list2.append(f(x))
+# #dont need 
+# # print("list2: ", list2)
+
+# #can use with top function to simplify
+# list2 =[f(x) for x in list1]
+# print("List2: ", list2)
+
+#can use by itself 
+# 
+
+
+# def total(arg1, arg2, arg3=10, arg4=20):
+#     return(arg1 ** arg2) + arg3 + arg4
+
+
+# # print(total(2,3))
+# # print(total(2, 3, 4))
+
+
+
+def total(arg1, arg2, arg3=10 arg4=20)
+    arg1 = arg1 + 10
+    arg2 = arg2 + 10
+    print("from within total() arg1: ", arg1)
+    print("from within total() arg2: ", arg2)
+    return (arg1 ** arg2) + arg3 + arg4
+
+arg1 = 2
+arg2 = 3
+print("from outside total() arg1: ", arg1)
+print("from outside total() arg2: ", arg2)
+
+
+
